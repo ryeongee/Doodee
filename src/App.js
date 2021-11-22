53fb8e24eaf9ac4c9e795303f770e260755429d3
ORG <47654977+ryeongee@users.noreply.github.com>
2021-11-22 23:50:43 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router';
+import { Route, Routes } from 'react-router';
 import Header from './pages/Header';
 import Estimates from './pages/Estimates';
 import Home from './pages/Home';
@@ -15,14 +15,16 @@ function App() {
   return (
     <>
       <Header />
-      <Route exact path='/' component={Home} />
-      <Route path='/process' component={Process} />
-      <Route path='/projects' component={Projects} />
-      <Route path='/housing' component={Housing} />
-      <Route path='/commercial' component={Commercial} />
-      <Route path='/estimates' component={Estimates} />
-      <Route path='/contact' component={Contact} />
-      <Route path='/view/1' component={View1} />
+      <Routes>
+        <Route exact path='/' component={Home} />
+        <Route path='/process' component={Process} />
+        <Route path='/projects' component={Projects} />
+        <Route path='/housing' component={Housing} />
+        <Route path='/commercial' component={Commercial} />
+        <Route path='/estimates' component={Estimates} />
+        <Route path='/contact' component={Contact} />
+        <Route path='/view/1' component={View1} />
+      </Routes>
       <Footer/>
     </>
   );
